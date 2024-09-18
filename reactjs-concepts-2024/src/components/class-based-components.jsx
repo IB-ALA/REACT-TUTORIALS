b45ee6fc7a3915014f7c707508b6eb725ce658c7
IBALA <ishaqibrahimyusif@gmail.com>
2024-09-18 11:59:22 +0000

    Class based components.

---
+++ reactjs-concepts-2024/src/components/class-based-components.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClassBasedComponents extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Class based component</h4>
+      </div>
+    );
+  }
+}
+
+export default ClassBasedComponents;
