d5152ac4d2c92ad755c5f71796277c7606569fe5
IBALA <ishaqibrahimyusif@gmail.com>
2024-09-19 11:20:25 +0000

    Conditional statements.

--- reactjs-concepts-2024/src/components/products/index.jsx
+++ reactjs-concepts-2024/src/components/products/index.jsx
@@ -4,17 +4,47 @@ import "./style.css";
 // nested components
 function ProductList({ name, city, listOfProducts }) {
   // console.log(props);
-
   // const { name, city } = props;
+
+  const flag = false;
+
+  function renderTextBlock(flag) {
+    return flag ? (
+      <h4>
+        Name is {name} and he belongs to {city} city
+      </h4>
+    ) : (
+      <h4>Hello world</h4>
+    );
+  }
+
+  // const renderTextBlock = flag ? (
+  //   <h4>
+  //     Name is {name} and he belongs to {city} city
+  //   </h4>
+  // ) : (
+  //   <h4>Hello world</h4>
+  // );
+
+  // let renderTextBlock = null;
+  // if (flag) {
+  //   renderTextBlock = (
+  //     <h4>
+  //       Name is {name} and he belongs to {city} city
+  //     </h4>
+  //   );
+  // } else {
+  //   renderTextBlock = <h4>Hello world</h4>;
+  // }
 
   return (
     <div>
       <h3 className="title">ECommerce Project</h3>
       {/* <ProductItem /> */}
 
-      <h4>
-        Name is {name} and he belongs to {city} city
-      </h4>
+      {/* conditional rendering */}
+      {renderTextBlock(flag)}
+      {/* {renderTextBlock} */}
 
       <ul>
         {listOfProducts.map((item, index) => (
