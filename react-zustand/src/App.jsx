27ceaad9896f32b2c4358951f775ce842917fa2d
IBALA <ishaqibrahimyusif@gmail.com>
2024-09-30 13:40:59 +0000

    Done with zustand lesson.

--- react-zustand/src/App.jsx
+++ react-zustand/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//
+
+import CounterValue from "./counter-app/counter-value";
+import ManageCounter from "./counter-app/manage-counter";
+import Products from "./products";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>React with zuzstand</h1>
+
+      <ManageCounter />
+      <CounterValue />
+
+      <Products />
+    </div>
+  );
 }
 
-export default App
+export default App;
