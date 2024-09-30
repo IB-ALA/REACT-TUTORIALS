ce255d3b1f9c65f981c7e211a88a345307ad04ac
IBALA <ishaqibrahimyusif@gmail.com>
2024-09-30 16:19:27 +0000

    React unit testing using Jest and React Testinng Library. (Starter files)

---
+++ react-testing/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HelloWorld from "./components/helloWorld";
+
+function App() {
+  return (
+    <div>
+      <h1>React Unit Testing Crash course</h1>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
